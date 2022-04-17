30bc69988357f4ccdd047671cb2103fecbdd1eb3
minhye0k <omh34658981@gmail.com>
2022-04-17 22:36:10 +0900

    study : custom bean init, destroy 1. Bean initMethod 2. Bean destroyMethod

--- spring5-chap06/src/main/java/chap06/AppContext.java
+++ spring5-chap06/src/main/java/chap06/AppContext.java
@@ -3,6 +3,7 @@ package chap06;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import spring.Client;
+import spring.Client2;
 
 @Configuration
 public class AppContext {
@@ -10,6 +11,13 @@ public class AppContext {
     @Bean
     public Client client() {
         Client client = new Client();
+        client.setHost("host");
+        return client;
+    }
+
+    @Bean(initMethod = "connect", destroyMethod = "close")
+    public Client2 client2() {
+        Client2 client = new Client2();
         client.setHost("host");
         return client;
     }
