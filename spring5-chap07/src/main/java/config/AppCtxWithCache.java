72582cf9b358d146dcd77ac9543a687d87fbc2ba
minhye0k <omh34658981@gmail.com>
2022-04-24 02:52:39 +0900

    study : proxy order and @Order

--- spring5-chap07/src/main/java/config/AppCtxWithCache.java
+++ spring5-chap07/src/main/java/config/AppCtxWithCache.java
@@ -1,5 +1,6 @@
 package config;
 
+import aspect.CacheAspect;
 import aspect.ExeTimeAspect;
 import chap07.Calculator;
 import chap07.RecCalculator;
@@ -8,8 +9,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
-@EnableAspectJAutoProxy(proxyTargetClass = true)
-public class AppCtx {
+@EnableAspectJAutoProxy
+public class AppCtxWithCache {
+
+    @Bean
+    public CacheAspect cacheAspect() {
+        return new CacheAspect();
+    }
 
     @Bean
     public ExeTimeAspect exeTimeAspect() {
--- spring5-chap07/src/main/java/main/MainAspect.java
+++ spring5-chap07/src/main/java/main/MainAspect.java
@@ -1,6 +1,6 @@
 package main;
 
-import chap07.Calculator;
+import chap07.RecCalculator;
 import config.AppCtx;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -9,7 +9,7 @@ public class MainAspect {
         AnnotationConfigApplicationContext ctx =
                 new AnnotationConfigApplicationContext(AppCtx.class);
 
-        Calculator cal = ctx.getBean("calculator", Calculator.class);
+        RecCalculator cal = ctx.getBean("calculator", RecCalculator.class);
         long fiveFact = cal.factorial(5);
         System.out.println("cal.factorial(5) = " + fiveFact);
         System.out.println(cal.getClass().getName());
