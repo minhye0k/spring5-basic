e77436c86b11dde1bf7b2d993e7c5b3f15fddb33
minhye0k <omh34658981@gmail.com>
2022-05-05 23:55:09 +0900

    study : make query execute method using jdbcTemplate

--- spring5-chap08/src/main/java/config/AppCtx.java
+++ spring5-chap08/src/main/java/config/AppCtx.java
@@ -1,5 +1,31 @@
 package config;
 
 
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import spring.MemberDao;
+
+@Configuration
 public class AppCtx {
+
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() {
+        DataSource ds = new DataSource();
+        ds.setDriverClassName("org.mariadb.jdbc.Driver");
+        ds.setUrl("jdbc:mariadb://localhost/spring5fs?characterEncoding=utf8");
+        ds.setUsername("spring5");
+        ds.setPassword("spring5");
+        ds.setInitialSize(2); // 초기 커넥션 개수
+        ds.setMaxActive(10); // 동시 가져 올 수 있는 커넥션 개수
+        ds.setTestWhileIdle(true); // 유휴 커넥션 검사
+        ds.setMinEvictableIdleTimeMillis(1000 * 60 * 3); // 최소 유휴 시간 3분
+        ds.setTimeBetweenEvictionRunsMillis(1000 * 10); // 10초 주기
+        return ds;
+    }
+
+    @Bean
+    public MemberDao memberDao() {
+        return new MemberDao(dataSource());
+    }
 }
--- spring5-chap08/src/main/java/spring/MemberDao.java
+++ spring5-chap08/src/main/java/spring/MemberDao.java
@@ -1,20 +1,69 @@
 package spring;
 
-import java.util.Collection;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 public class MemberDao {
+    private JdbcTemplate jdbcTemplate;
+
+    public MemberDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
     public Member selectByEmail(String email) {
-        return null;
+        List<Member> results = jdbcTemplate.query(
+                "select * from MEMBER where EMAIL = ?",
+                new RowMapper<Member>() {
+                    @Override
+                    public Member mapRow(ResultSet resultSet, int i) throws SQLException {
+                        Member member = new Member(
+                                resultSet.getString("EMAIL"),
+                                resultSet.getString("PASSWORD"),
+                                resultSet.getString("NAME"),
+                                resultSet.getTimestamp("REGDATE").toLocalDateTime());
+                        member.setId(resultSet.getLong("ID"));
+                        return member;
+                    }
+                }, email);
+        return results.isEmpty() ? null : results.get(0);
     }
 
     public void insert(Member member) {
     }
 
     public void update(Member member) {
-
+        jdbcTemplate.update(
+                "update MEMBER set NAME = ?, PASSWORD = ? where EMAIL = ?",
+                member.getName(), member.getPassword(), member.getEmail()
+        );
     }
 
-    public Collection<Member> selectAll() {
-        return null;
+    public List<Member> selectAll() {
+        List<Member> results = jdbcTemplate.query(
+                "select * from MEMBER",
+                (resultSet, i) -> {
+                    Member member = new Member(
+                            resultSet.getString("EMAIL"),
+                            resultSet.getString("PASSWORD"),
+                            resultSet.getString("NAME"),
+                            resultSet.getTimestamp("REGDATE").toLocalDateTime());
+                    member.setId(resultSet.getLong("ID"));
+                    return member;
+                });
+
+        return results;
     }
+
+    public int count() {
+        Integer count = jdbcTemplate.queryForObject(
+                "select count(*) from MEMBER", Integer.class);
+
+        return count;
+    }
+
 }
