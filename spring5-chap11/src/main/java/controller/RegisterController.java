a0204ab0ccf1bf2840b93243e478e0ed63d74ddc
minhye0k <omh34658981@gmail.com>
2022-06-05 17:47:15 +0900

    Add Register Step 2

--- spring5-chap11/src/main/java/controller/RegisterController.java
+++ spring5-chap11/src/main/java/controller/RegisterController.java
@@ -1,12 +1,24 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class RegisterController {
     @RequestMapping("/register/step1")
     public String handleStep1() {
         return "register/step1";
+    }
+
+    @PostMapping("/register/step2")
+    public String handleStep2(HttpServletRequest request) {
+        String agreeParam = request.getParameter("agree");
+        if (agreeParam == null || !agreeParam.equals("true")) {
+            return "register/step1";
+        }
+        return "register/step2";
     }
 }
