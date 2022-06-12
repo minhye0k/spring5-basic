ade8ff0f8b7b62e80bf5583ab321a3aa70a8fac5
minhye0k <omh34658981@gmail.com>
2022-06-12 18:28:48 +0900

    study: spring validator

--- spring5-chap12/src/main/java/controller/RegisterController.java
+++ spring5-chap12/src/main/java/controller/RegisterController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,11 +39,15 @@ public class RegisterController {
     }
 
     @PostMapping("/register/step3")
-    public String handleStep3(RegisterRequest regReq) {
+    public String handleStep3(RegisterRequest regReq, Errors errors) {
+        new RegisterRequestValidator().validate(regReq, errors);
+        if (errors.hasErrors())
+            return "register/step2";
         try {
             memberRegisterService.regist(regReq);
             return "register/step3";
         } catch (DuplicateMemberException ex) {
+            errors.rejectValue("email", "duplicate");
             return "register/step2";
         }
     }
