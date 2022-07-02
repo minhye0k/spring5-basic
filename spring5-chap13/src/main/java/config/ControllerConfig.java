ffc90b3c22fcbb31840778c7654f75ae080c7092
minhye0k <omh34658981@gmail.com>
2022-07-02 20:24:49 +0900

    add feature of changing password

--- spring5-chap13/src/main/java/config/ControllerConfig.java
+++ spring5-chap13/src/main/java/config/ControllerConfig.java
@@ -1,5 +1,6 @@
 package config;
 
+import controller.ChangePwdController;
 import controller.LoginController;
 import controller.LogoutController;
 import controller.RegisterController;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import spring.AuthService;
+import spring.ChangePasswordService;
 import spring.MemberRegisterService;
 import survey.SurveyController;
 
@@ -16,6 +18,9 @@ public class ControllerConfig {
     private MemberRegisterService memberRegSvc;
     @Autowired
     private AuthService authService;
+
+    @Autowired
+    private ChangePasswordService changePasswordService;
 
     @Bean
     public RegisterController registerController() {
@@ -39,5 +44,12 @@ public class ControllerConfig {
     @Bean
     public LogoutController logoutController() {
         return new LogoutController();
+    }
+
+    @Bean
+    public ChangePwdController changePwdController() {
+        ChangePwdController controller = new ChangePwdController();
+        controller.setChangePasswordService(changePasswordService);
+        return controller;
     }
 }
