0520b9ddc2dd1acdfe3ee25eac12d26ada11b03b
minhye0k <omh34658981@gmail.com>
2022-07-02 19:19:20 +0900

    chap 13. add login feature(controller, service, password matches, view pages) ;

---
+++ spring5-chap13/src/main/java/config/ControllerConfig.java
@@ -0,0 +1,37 @@
+package config;
+
+import controller.LoginController;
+import controller.RegisterController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import spring.AuthService;
+import spring.MemberRegisterService;
+import survey.SurveyController;
+
+@Configuration
+public class ControllerConfig {
+    @Autowired
+    private MemberRegisterService memberRegSvc;
+    @Autowired
+    private AuthService authService;
+
+    @Bean
+    public RegisterController registerController() {
+        RegisterController controller = new RegisterController();
+        controller.setMemberRegisterService(memberRegSvc);
+        return controller;
+    }
+
+    @Bean
+    public SurveyController surveyController() {
+        return new SurveyController();
+    }
+
+    @Bean
+    public LoginController loginController() {
+        LoginController loginController = new LoginController();
+        loginController.setAuthService(authService);
+        return loginController;
+    }
+}
